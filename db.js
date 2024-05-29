f952e698b2ceae2c55ec69a99e134f8735fe8c7d
Shagun Verma <shagun.verma@lalamove.com>
2024-05-29 17:51:19 +0530

    Added new code for db deployemnet

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
+	host: 'mysql-normal',
+	user: 'root',
+	password: 'root',
+	database: 'todo',
 });
 
 con.connect(function (err) {
