c350d7292f70635f788c62ed6116e8f5159e04ae
Shagun Verma <shagun.verma@lalamove.com>
2024-05-31 14:21:10 +0530

    database url  changed

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-	host: 'mysql-normal',
-	user: 'root',
-	password: 'root',
-	database: 'todo',
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
 });
 
 con.connect(function (err) {
